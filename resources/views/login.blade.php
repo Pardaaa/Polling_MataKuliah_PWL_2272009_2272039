13061fa7fc78e202f8c017f5f8b07a9daaca6fa9
Pardaaa <rae.hutagalung@gmail.com>
2024-03-19 23:46:56 +0700

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -78,12 +78,12 @@
             <div class="card-text">
                 <form>
                     <div class="mb-3">
-                        <label for="exampleInputEmail1" class="form-label">Username</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                        <label for="email" class="form-label">Email</label>
+                        <input type="email" class="form-control" name="" id="email" >
                     </div>
                     <div class="mb-3">
-                        <label for="exampleInputPassword1" class="form-label">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1">
+                        <label for="password" class="form-label">Password</label>
+                        <input type="password" class="form-control" name="password" id="exampleInputPassword1">
                     </div>
                     <div class=" gap-2 d-flex">
                         <button type="submit" class="btn login">Login</button>
