8cbafef4928e9be3d83fbdeae289794abba8a091
Pardaaa <rae.hutagalung@gmail.com>
2024-03-24 19:58:18 +0700

    Update mahasiswa.blade.php

--- resources/views/Mahasiswa/mahasiswa.blade.php
+++ resources/views/Mahasiswa/mahasiswa.blade.php
@@ -18,7 +18,7 @@
     <div class="container-fluid">
         <div class="row mb-2">
             <div class="col-sm-6">
-                <h1 class="m-0">Kartu Keluarga Form</h1>
+                <h1 class="m-0">INI MAHASISWA</h1>
             </div><!-- /.col -->
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
