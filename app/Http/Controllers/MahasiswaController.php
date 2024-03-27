df2232006ffc7475e1bb76c871bdb90c178c8499
AlvinAugusF <114635249+AlvinAugusF@users.noreply.github.com>
2024-03-27 16:56:29 +0700

    Penambahan Auth dan Login berdasarkan role

---
+++ app/Http/Controllers/MahasiswaController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MahasiswaController extends Controller
+{
+    public function dashboard()
+    {
+        return view('Mahasiswa.mahasiswa');
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
