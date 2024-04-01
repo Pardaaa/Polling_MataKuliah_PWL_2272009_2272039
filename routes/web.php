96858f46f579ecd61527b80fc213c371773f0d9e
Charles Winata <113487745+2272009@users.noreply.github.com>
2024-04-02 00:11:12 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,14 +17,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+Route::get('/add', [\App\Http\Controllers\HomeController::class, 'add'])->name('add');
+Route::post('/add', [\App\Http\Controllers\HomeController::class, 'save'])->name('save');
+Auth::routes();
 
-require __DIR__.'/auth.php';
+Route::get('datamahasiswa', function () { return view('layouts\mahasiswa\datamahasiswa'); })->middleware('checkRole:admin');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/datamahasiswa', [App\Http\Controllers\MahasiswaController::class, 'index'])->name('datamahasiswa');
+Route::get('admin', function () { return view('layouts\admin\admin'); })->middleware('checkRole:admin');
+Route::get('prodi', function () { return view('layouts\prodi\prodi'); })->middleware(['checkRole:prodi,admin']);
+Route::get('mahasiswa', function () { return view('layouts\mahasiswa\mahasiswa'); })->middleware(['checkRole:mahasiswa,admin']);
+Route::get('polling', function () { return view('layouts\admin\polling'); })->middleware('checkRole:admin');
+Route::delete('hapus/{id}', [App\Http\Controllers\MahasiswaController::class, 'hapus'])->name('hapus');
