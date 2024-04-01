6d503359026e074d19905108d1f130aa6fea958c
Charles Winata <113487745+2272009@users.noreply.github.com>
2024-04-02 00:04:50 +0700

    Update MahasiswaController.php

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -2,25 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Mahasiswa;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class MahasiswaController extends Controller
 {
-    public function dashboard()
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
     {
-        return view('Mahasiswa.mahasiswa');
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $data = Mahasiswa::where('role', 'mahasiswa')->get();
+        return view('layouts\mahasiswa\datamahasiswa', compact('data'));
+    }
+
+    public function hapus($id)
+    {
+        $data = Mahasiswa::find($id);
+        Mahasiswa::where('id', $id)->delete();
+        return redirect('datamahasiswa')->with('success', 'Data berhasil di hapus!');
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
