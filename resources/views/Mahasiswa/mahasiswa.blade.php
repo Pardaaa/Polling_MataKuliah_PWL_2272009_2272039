1694b9d50b1f5ffbc68a2b2ad63c10787fe069cf
Pardaaa <rae.hutagalung@gmail.com>
2024-04-04 20:09:22 +0700

    adminlte

---
+++ resources/views/Mahasiswa/mahasiswa.blade.php
@@ -0,0 +1,72 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Mahasiswa</title>
+    {{--  CSS Bootstrap  --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    {{--  Google Font  --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+</head>
+<body>
+<div class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1 class="m-0">INI MAHASISWA</h1>
+            </div><!-- /.col -->
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                    <li class="breadcrumb-item active">Kartu Keluarga Form</li>
+                </ol>
+            </div><!-- /.col -->
+        </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+</div>
+<!-- /.content-header -->
+
+<!-- Main content -->
+<div class="content">
+    <div class="container-fluid">
+        @if($errors->any())
+            <div class="alert alert-danger">
+                {{ implode('', $errors->all(':message')) }}
+            </div>
+        @endif
+        <div class="card p-4">
+            <form method="post" action="">
+                @csrf
+                <div class="card-body">
+                    <div class="form-group">
+                        <label for="kk-id">Nomor Kartu Keluarga</label>
+                        <input type="text" class="form-control" id="kk-id" placeholder="Nomor KK" name="id" required autofocus maxlength="15">
+                    </div>
+                    <div class="form-group">
+                        <label for="nama-kk">Nama Kepala Keluarga</label>
+                        <input type="text" maxlength="100" class="form-control" id="nama-kk" placeholder="Contoh: John Doe" required name="kepala_keluarga">
+                    </div>
+                </div>
+
+                <div class="card-footer">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div><!-- /.container-fluid -->
+<!-- /.content -->
+</body>
+
+{{--  JS Bootstrap  --}}
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</html>
+
+<!-- Content Header (Page header) -->
+
+
