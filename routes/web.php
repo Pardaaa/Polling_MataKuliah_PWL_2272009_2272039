da34cc2329f1dc7fe68fcd28fd070fa1af0076c4
Pardaaa <rae.hutagalung@gmail.com>
2024-04-14 17:54:11 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
 });
 
 Route::get('/add', [\App\Http\Controllers\HomeController::class, 'add'])->name('add');
