afa99ababe845e0130fa2b499f69037241d86d56
Pardaaa <rae.hutagalung@gmail.com>
2024-04-16 00:09:19 +0700

    Update datamatakuliahadmin.blade.php

--- resources/views/layouts/admin/datamatakuliahadmin.blade.php
+++ resources/views/layouts/admin/datamatakuliahadmin.blade.php
@@ -81,7 +81,7 @@
                 <i class="fas fa-fw fa-vote-yea"></i>
                 Voting</a>
 
-            <a class="nav-link" href="index.html">
+            <a class="nav-link" href="hasilPollingadmin">
                 <i class="fas fa-fw fa-poll"></i>
                 Hasil Polling</a>
         </li>
@@ -99,12 +99,9 @@
 
                 <!-- Page Heading -->
                 <br>
-                <h1 class="h3 mb-2 text-gray-800">Data Matakuliah</h1>
+                <h1 class="h3 mb-2 text-gray-800">Data Mata kuliah</h1>
                 <!-- DataTales Example -->
                 <div class="card shadow mb-4">
-                    <div class="card-header py-3">
-                        <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
-                    </div>
                     <div class="card-body">
                         <div class="table-responsive">
                         <a href="addmatakuliahadmin" class="btn btn-primary mb-3">Tambah Data</a>
