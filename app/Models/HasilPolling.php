11179e551991ea9aab61061f8e1e6aa9f3c60850
Pardaaa <rae.hutagalung@gmail.com>
2024-04-15 10:20:57 +0700

    Update HasilPolling.php

--- app/Models/HasilPolling.php
+++ app/Models/HasilPolling.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class HasilPolling extends Model
 {
     protected $table = 'hasilpolling';
+    use HasFactory;
 
     protected $fillable  = [
         'id',
