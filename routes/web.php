bc36d133525aec32e3346ecd5baa4f93327019c4
Charles Winata <113487745+2272009@users.noreply.github.com>
2024-04-15 15:17:48 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect('login');
+    return redirect('home');
 });
 
 Route::get('/add', [\App\Http\Controllers\HomeController::class, 'add'])->name('add');
