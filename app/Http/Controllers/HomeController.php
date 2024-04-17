299c12162e24e2b40b5143afc920f127b55af69d
Charles Winata <113487745+2272009@users.noreply.github.com>
2024-04-17 20:46:54 +0700

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -29,36 +29,4 @@ class HomeController extends Controller
         return view('home');
     }
 
-    public function add()
-    {
-        return view('layouts\admin\add');
-    }
-
-    public function save(Request $request)
-    {
-        $this->validate($request, [
-            'id' => 'required|min:7|unique:users,id|numeric',
-            'name' => 'required|min:3',
-            'email' =>'required',
-            'password' => 'required|min:6',
-            'role' => 'required'
-        ]);
-
-        $id = $request->id;
-        $nama = $request->name;
-        $email = $request->email;
-        $password = $request->password;
-        $hashedPassword = Hash::make($password);
-        $role = $request->role;
-
-        $data = new Mahasiswa;
-        $data->id = $id;
-        $data->name = $nama;
-        $data->email = $email;
-        $data->password = $hashedPassword;
-        $data->role = $role;
-        $data->save();
-
-        return redirect('datamahasiswadankandidat')->with('success', 'Data berhasil di tambah!');
-    }
 }
