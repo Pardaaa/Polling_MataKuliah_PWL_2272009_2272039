ecde43c1fe50eed0535b1b19c18e8f99cb2d6479
AlvinAugusF <114635249+AlvinAugusF@users.noreply.github.com>
2024-04-19 14:48:30 +0700

    update add.blade.php

--- resources/views/layouts/admin/add.blade.php
+++ resources/views/layouts/admin/add.blade.php
@@ -211,6 +211,22 @@
         });
     });
 </script>
+<script>
+    document.getElementById('togglePassword').addEventListener('click', function() {
+        const passwordInput = document.getElementById('password');
+        const icon = document.getElementById('togglePassword');
+
+        if (passwordInput.type === "password") {
+            passwordInput.type = "text";
+            icon.classList.remove('fa-eye');
+            icon.classList.add('fa-eye-slash');
+        } else {
+            passwordInput.type = "password";
+            icon.classList.remove('fa-eye-slash');
+            icon.classList.add('fa-eye');
+        }
+    });
+</script>
 </body>
 </html>
 @endsection
