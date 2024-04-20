14028c530a1ee252d47d60fd53539ec7a5e0e3be
AlvinAugusF <114635249+AlvinAugusF@users.noreply.github.com>
2024-04-20 18:27:11 +0700

    update addpolling prodi

--- resources/views/layouts/prodi/addpolling.blade.php
+++ resources/views/layouts/prodi/addpolling.blade.php
@@ -78,6 +78,10 @@
             <a class="nav-link" href="hasilpolling">
                 <i class="fas fa-fw fa-poll"></i>
                 Hasil Polling</a>
+
+            <a class="nav-link" href="/change-passwordprodi">
+                <i class="fas fa-fw fa-lock"></i>
+                Change Password</a>
         </li>
         </ul>
         <!-- End of Sidebar -->
