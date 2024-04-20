d7efd31d3c408602a7ff4371ba6a398694d3a045
AlvinAugusF <114635249+AlvinAugusF@users.noreply.github.com>
2024-04-20 18:27:55 +0700

    add password prodi

---
+++ resources/views/layouts/prodi/passwordprodi.blade.php
@@ -0,0 +1,221 @@
+@extends('app')
+
+@section('content')
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>PASSWORD</title>
+
+    <!-- Custom fonts for this template -->
+    <link href="{{ asset('sbadmin/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="{{ asset('sbadmin/css/sb-admin-2.min.css') }}" rel="stylesheet">
+
+    <!-- Custom styles for this page -->
+    <link href="{{ asset('sbadmin/vendor/datatables/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
+
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+</head>
+
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+            <!-- Sidebar - Brand -->
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="mahasiswa">
+                <div class="sidebar-brand-icon rotate-n-15">
+                    <i class="fas fa-laugh-wink"></i>
+                </div>
+                <div class="sidebar-brand-text mx-3">Polling</div>
+            </a>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider my-0">
+
+{{--            <!-- Nav Item - Dashboard -->--}}
+            <li class="nav-item">
+                <a class="nav-link" href="prodi">
+                    <i class="fas fa-fw fa-tachometer-alt"></i>
+                    Dashboard</a>
+            </li>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider my-0">
+
+            <!-- Heading -->
+            <div class="sidebar-heading">
+                Menu Data Master
+            </div>
+
+            <!-- Nav Item -->
+            <li class="nav-item">
+                <a class="nav-link" href="datamahasiswa">
+                    <i class="fas fa-fw fa-user-graduate"></i>
+                    Data Mahasiswa</a>
+
+                <a class="nav-link" href="datamatakuliah">
+                    <i class="fas fa-fw fa-book-dead"></i>
+                    Data Mata Kuliah</a>
+
+                <a class="nav-link" href="periode">
+                    <i class="fas fa-fw fa-calendar"></i>
+                    Setting Periode</a>
+
+                <a class="nav-link" href="hasilpollingprodi">
+                    <i class="fas fa-fw fa-poll"></i>
+                    Hasil Polling</a>
+
+                <a class="nav-link" href="/change-passwordprodi">
+                    <i class="fas fa-fw fa-lock"></i>
+                    Change Password</a>
+            </li>
+        </ul>
+        <!-- End of Sidebar -->
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+                    <!-- Page Heading -->
+                    <br>
+                    <div class="row justify-content-center">
+                        <div class="col-md-6">
+                            <form method="POST" action="{{ route('changepasswordprodi') }}">
+                                @csrf
+
+                                <div class="mb-3">
+                                    <label for="current_password" class="form-label">Password Saat Ini</label>
+                                    <div class="input-group">
+                                        <input type="password" name="current_password" id="current_password" class="form-control" required>
+                                        <div class="input-group-append">
+                                            <button type="button" class="btn btn-outline-secondary toggle-password" data-target="current_password">
+                                                <i class="fas fa-eye"></i>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="mb-3">
+                                    <label for="new_password" class="form-label">Password Baru</label>
+                                    <div class="input-group">
+                                        <input type="password" name="new_password" id="new_password" class="form-control" required>
+                                        <div class="input-group-append">
+                                            <button type="button" class="btn btn-outline-secondary toggle-password" data-target="new_password">
+                                                <i class="fas fa-eye"></i>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="new_password_confirmation" class="form-label">Konfirmasi Password Baru</label>
+                                    <div class="input-group">
+                                        <input type="password" name="new_password_confirmation" id="new_password_confirmation" class="form-control" required>
+                                        <div class="input-group-append">
+                                            <button type="button" class="btn btn-outline-secondary toggle-password" data-target="new_password_confirmation">
+                                                <i class="fas fa-eye"></i>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <button type="submit" class="btn btn-primary">Ubah Password</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <!-- /.container-fluid -->
+
+            </div>
+            <!-- End of Main Content -->
+
+            <!-- Footer -->
+            <footer class="sticky-footer bg-white">
+                <div class="container my-auto">
+                    <div class="copyright text-center my-auto">
+                        <span>Copyright &copy; Tugas Pemrograman Web Lanjut 2024</span>
+                    </div>
+                </div>
+            </footer>
+            <!-- End of Footer -->
+
+        </div>
+        <!-- End of Content Wrapper -->
+
+    </div>
+    <!-- End of Page Wrapper -->
+
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="{{ asset('sbadmin/vendor/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('sbadmin/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="{{ asset('sbadmin/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="{{ asset('sbadmin/js/sb-admin-2.min.js') }}"></script>
+
+    <!-- Page level plugins -->
+    <script src="{{ asset('sbadmin/vendor/datatables/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('sbadmin/vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="{{ asset('sbadmin/js/demo/datatables-demo.js') }}"></script>
+
+    <!-- SweetAlert2 -->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            function togglePasswordVisibility(targetId) {
+                const targetInput = document.getElementById(targetId);
+                const icon = document.querySelector(`[data-target="${targetId}"] i`);
+
+                if (targetInput.type === "password") {
+                    targetInput.type = "text";
+                    icon.classList.remove('fa-eye');
+                    icon.classList.add('fa-eye-slash');
+                } else {
+                    targetInput.type = "password";
+                    icon.classList.remove('fa-eye-slash');
+                    icon.classList.add('fa-eye');
+                }
+            }
+
+            const togglePasswordButtons = document.querySelectorAll('.toggle-password');
+            togglePasswordButtons.forEach(button => {
+                button.addEventListener('click', function() {
+                    const targetId = this.getAttribute('data-target');
+                    togglePasswordVisibility(targetId);
+                });
+            });
+        });
+    </script>
+</body>
+
+</html>
+@endsection
