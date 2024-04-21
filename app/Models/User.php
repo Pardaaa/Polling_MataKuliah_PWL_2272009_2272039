f986c9e5a38dd41bb33623b5e0d9dac66d9a51de
Charles Winata <113487745+2272009@users.noreply.github.com>
2024-04-21 19:06:18 +0700

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -11,8 +10,41 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+
     protected $table = 'users';
 
+    /**
+     * Determine if the user is an admin.
+     *
+     * @return bool
+     */
+    public function isAdmin()
+    {
+        // Implement your admin role logic here
+        return $this->role === 'admin';
+    }
+
+    /**
+     * Determine if the user is in prodi role.
+     *
+     * @return bool
+     */
+    public function isProdi()
+    {
+        // Implement your prodi role logic here
+        return $this->role === 'prodi';
+    }
+
+    /**
+     * Determine if the user is in mahasiswa role.
+     *
+     * @return bool
+     */
+    public function isMahasiswa()
+    {
+        // Implement your mahasiswa role logic here
+        return $this->role === 'mahasiswa';
+    }
 
     public function hasilPolling()
     {
