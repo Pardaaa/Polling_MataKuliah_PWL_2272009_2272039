5012b20e684badec469b225e597d0e5969b487ac
Charles Winata <113487745+2272009@users.noreply.github.com>
2024-04-21 17:18:54 +0700

    Update MahasiswaController.php

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -6,6 +6,7 @@ use App\Models\Mahasiswa;
 use App\Models\Polling;
 use App\Models\Matakuliah;
 use App\Models\HasilPolling;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
